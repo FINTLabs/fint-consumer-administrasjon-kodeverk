8a6b93ec8218d7609f27c2229bc58e7252a8ede0
Hknots <hknots99@gmail.com>
2024-05-21 12:13:04 +0200

    manual changes

--- src/main/java/no/fint/consumer/models/stillingskode/StillingskodeCacheService.java
+++ src/main/java/no/fint/consumer/models/stillingskode/StillingskodeCacheService.java
@@ -107,7 +107,10 @@ public class StillingskodeCacheService extends CacheService<StillingskodeResourc
         } else {
             data = objectMapper.convertValue(event.getData(), javaType);
         }
-        data.forEach(linker::mapLinks);
+        data.forEach(resource -> {
+            linker.mapLinks(resource);
+            linker.resetSelfLinks(resource);
+        });
         if (KodeverkActions.valueOf(event.getAction()) == KodeverkActions.UPDATE_STILLINGSKODE) {
             if (event.getResponseStatus() == ResponseStatus.ACCEPTED || event.getResponseStatus() == ResponseStatus.CONFLICT) {
                 List<CacheObject<StillingskodeResource>> cacheObjects = data
